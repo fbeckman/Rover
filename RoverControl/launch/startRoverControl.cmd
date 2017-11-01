ef66e607e61f8a928c73761b979b2a76a219c8d9
Frank Beckmann <fbeckman@t-online.de>
2017-11-01 20:54:27 +0100

    Some tools to stop and start the job on the raspberry pi

---
+++ RoverControl/launch/startRoverControl.cmd
@@ -0,0 +1,2 @@
+REM Start RoverControl job on ROSberrPi
+..\Tools\plink -ssh pi@ROSberryPi -pw pilot6301 /home/pi/start
---
+++ RoverControl/launch/stopRoverControl.cmd
@@ -0,0 +1,2 @@
+REM Stop RoverControl job on ROSberrPi
+..\Tools\plink -ssh pi@ROSberryPi -pw pilot6301 /home/pi/stop
